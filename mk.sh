0da222de0908d6581efa31c8322cd90cbdfa430c
samesense <samesense@gmail.com>
2017-05-18 22:59:30 -0400

    still fixing automation

--- mk.sh
+++ mk.sh
@@ -13,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -22,4 +22,4 @@ echo "Generating site"
 hugo --theme hyde
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (mk.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (mk.sh)" && git push origin gh-pages
